ee6f8900001dc0d30aadd19e1263d7d293bb9809
Rocky Li <liqiuyi1991@gmail.com>
2018-05-01 21:30:44 +0800

    removed __macosx

--- lstm.py
+++ lstm.py
@@ -7,3 +7,5 @@ dataPath = os.path.join(os.getcwd(), 'local.csv')
 
 dateParser = lambda x: pd.datetime.fromtimestamp(float(x))
 series = pd.read_csv(dataPath, usecols=['TIME', 'Y'], index_col=0, squeeze=True, parse_dates=['TIME'], date_parser=dateParser)
+
+
